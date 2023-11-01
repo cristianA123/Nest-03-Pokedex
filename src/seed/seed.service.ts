b7312f0a3474f8c601adfaca7c79ca8f5668483a
cristianA123 <cristianchipan2@gmail.com>
2023-11-01 17:36:56 -0500

    Se agrego datos con una promesa, Promise.all()

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -18,21 +18,24 @@ export class SeedService {
 
   async executeSeed() {
 
-    const {data} = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=10`)
+    await this.pokemonModel.deleteMany({});
 
+    const {data} = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650`)
 
+    const pokemonToInsert: { name: string, no: number }[] = [];
 
+    data.results.forEach( ({name, url} ) => {
 
-    data.results.forEach( async ({name, url} ) => {
       const segments = url.split('/');
       const no =+ segments[segments.length -2];
 
-      console.log({name, no})
-      await this.pokemonModel.create({name, no});
+      pokemonToInsert.push({ name, no })
 
     });
 
-    return data.results;
+    await this.pokemonModel.insertMany(pokemonToInsert)
+
+    return 'Seed excuted';
   }
 
  
